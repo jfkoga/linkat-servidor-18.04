060e544a0dd628b65ebd0ed81c3640427226e163
Pablo Vigo <pvigo@xtec.cat>
2018-11-12 10:55:36 +0100

    49: restore/new folders(smb/nfs)/calibre folder

--- linkat-servidor/plantilles/calibre-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.py | sudo python -c "import sys; main=lambda x:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main('/opt')"
-
-
