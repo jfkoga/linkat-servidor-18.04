df1b42acbaba47fdf9eb9d2ce022af34f23845ed
Francesc Konate <jfkoga@gmail.com>
2019-07-22 13:54:43 +0200

    106:[FIX]chmod 750 -R /etc/linkat/linkat-servidor/patches/;# Restaura la plantilla per defecte de nextcloud-restart

--- debian/linkat-servidor/etc/linkat/linkat-servidor/patches/01-nextcloud.sh
+++ debian/linkat-servidor/etc/linkat/linkat-servidor/patches/01-nextcloud.sh
@@ -3,8 +3,13 @@
 NAME="$(grep NEW_NAME /etc/linkat/linkat-servidor/linkat-servidor.conf | cut -d "=" -f2)"
 DOMAIN="$(grep DOMAIN /etc/linkat/linkat-servidor/linkat-servidor.conf | cut -d "=" -f2)"
 IP="$(grep IP /etc/linkat/linkat-servidor/linkat-servidor.conf | cut -d "=" -f2)"
+NCTEMPLATE="/usr/share/linkat/linkat-servidor/plantilles/nextcloud-restart.sh"
+NCRESTART="/usr/local/bin/nextcloud-restart.sh"
 
-# Comprova que Nextcloud restart té els paràmetres del Servidor de Centre
+# Restaura la plantilla per defecte de nextcloud-restart
+cp $NCTEMPLATE $NCRESTART
+
+# Assigna a la plantilla restaurada els paràmetres existents del Servidor de Centre
         if [ -f /usr/local/bin/nextcloud-restart.sh ]; then
                         sed -i "s/__NAME__/$NAME/g" /usr/local/bin/nextcloud-restart.sh
 			sed -i "s/__DOMAIN__/$DOMAIN/g" /usr/local/bin/nextcloud-restart.sh
