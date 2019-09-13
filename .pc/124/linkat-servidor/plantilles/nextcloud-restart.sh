c4d85cefd9fa5d67722d2f17d257acc4598391b3
Francesc Konate <jfkoga@tutanota.com>
2019-09-13 13:26:33 +0200

    124: Commented Apparmor statements /bzr/linkat-edu-18/linkat-servidor-18.04/linkat-servidor-18.04/linkat-servidor/configurador/roles/ldap/tasks/main.yml

--- .pc/124/linkat-servidor/plantilles/nextcloud-restart.sh
+++ .pc/124/linkat-servidor/plantilles/nextcloud-restart.sh
@@ -4,7 +4,7 @@
 
 zenity --width="250" --question --title="Reinici Nextcloud" --text="Voleu reiniciar el servei Nextcloud?"
 if [ $? -gt 0 ]; then
-	exit 1
+	exit 11
 else
 
 # Apparmor Nextcloud
@@ -12,7 +12,6 @@ apparmorLink="/etc/apparmor.d/disable/usr.lib.snapd.snap-confine.real"
 if [[ ! -e $apparmorLink ]]; then
         sudo ln -s /etc/apparmor.d/usr.lib.snapd.snap-confine.real /etc/apparmor.d/disable/
         sudo apparmor_parser -R /etc/apparmor.d/usr.lib.snapd.snap-confine.real
-	sudo systemctl restart snapd
 fi
 
 
